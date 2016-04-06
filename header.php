50c3c6b9aef8f6f8e203afb8762fc4f00752090a
Tom Hermans <tomhermans@gmail.com>
2016-04-06 17:57:06 +0200

    add form vars + favicons

--- header.php
+++ header.php
@@ -14,11 +14,27 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
-		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo THEME_URI; ?>/assets/images/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="<?php echo THEME_URI; ?>/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="<?php echo THEME_URI; ?>/assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="<?php echo THEME_URI; ?>/assets/images/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="<?php echo THEME_URI; ?>/assets/images/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="<?php echo THEME_URI; ?>/assets/images/favicons/manifest.json">
+		<link rel="mask-icon" href="<?php echo THEME_URI; ?>/assets/images/favicons/safari-pinned-tab.svg" color="#e32127">
+		<link rel="shortcut icon" href="<?php echo THEME_URI; ?>/assets/images/favicons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="<?php echo THEME_URI; ?>/assets/images/favicons/mstile-144x144.png">
+		<meta name="msapplication-config" content="<?php echo THEME_URI; ?>/assets/images/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#e32127">
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
